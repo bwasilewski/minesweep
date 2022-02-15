acc32a0abb62b2d5079b2dde105a5f9425b37ea6
Benjamin Wasilewski <benwasilewski@gmail.com>
2022-02-15 11:00:04 -0600

    add ticker / add sprite to stage

--- src/js/minesweep.js
+++ src/js/minesweep.js
@@ -1,6 +1,27 @@
 import * as Pi from 'pixi.js'
+import unclicked from '@/images/tile_unclicked.png'
+import clicked from '@/images/tile_clicked.png'
+
+
+const Update = app => {
+	let elapsed = 0.0
+
+	app.ticker.add((delta) => {
+		// console.log('D: ', delta)
+
+	})
+}
 
 export const AppView = () => {
-	const app = new Pi.Application()
+	const app = new Pi.Application({
+		width: 600,
+		height: 600
+	})
+	let unclickedSprite = Pi.Sprite.from(unclicked)
+	let clickedSprite = Pi.Sprite.from(clicked)
+
+	Update(app)
+
+	app.stage.addChild(unclickedSprite)
 	return app.view
 }
