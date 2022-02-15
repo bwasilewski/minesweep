06097bb3c27294a2e806845e285e3f8104f8e976
Benjamin Wasilewski <benwasilewski@gmail.com>
2022-02-14 18:06:00 -0600

    remove boilerplate content from homepage

--- src/index.js
+++ src/index.js
@@ -2,25 +2,26 @@
 import { example } from '@/js/example'
 
 // Test import of an asset
-import webpackLogo from '@/images/webpack-logo.svg'
+// import webpackLogo from '@/images/webpack-logo.svg'
 
 // Test import of styles
 import '@/styles/index.scss'
 
 // Appending to the DOM
-const logo = document.createElement('img')
-logo.src = webpackLogo
+// const logo = document.createElement('img')
+// logo.src = webpackLogo
 
 const heading = document.createElement('h1')
 heading.textContent = example()
 
 // Test a background image url in CSS
-const imageBackground = document.createElement('div')
-imageBackground.classList.add('image')
+// const imageBackground = document.createElement('div')
+// imageBackground.classList.add('image')
 
 // Test a public folder asset
-const imagePublic = document.createElement('img')
-imagePublic.src = '/assets/example.png'
+// const imagePublic = document.createElement('img')
+// imagePublic.src = '/assets/example.png'
 
 const app = document.querySelector('#root')
-app.append(logo, heading, imageBackground, imagePublic)
+// app.append(logo, heading, imageBackground, imagePublic)
+app.append(heading)
